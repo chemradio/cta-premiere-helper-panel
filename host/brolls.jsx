2c31d3b0104da704d4c95867fb83103b5cb4b3a8
chemradio <chemradio@gmail.com>
2023-05-26 11:42:14 +0600

    ...

--- host/brolls.jsx
+++ host/brolls.jsx
@@ -1,5 +1,6 @@
 function brollBoxGuest() {
-    var firstClipFirstTrackQE = qe.project.getActiveSequence().getVideoTrackAt(0).getItemAt(0)
-    firstClipFirstTrackQE.addVideoEffect(qe.project.getVideoEffectByName("Transform"));
+    // var firstClipFirstTrackQE = qe.project.getActiveSequence().getVideoTrackAt(0).getItemAt(0)
+    // firstClipFirstTrackQE.addVideoEffect(qe.project.getVideoEffectByName("Transform"));
+    
 
 }
